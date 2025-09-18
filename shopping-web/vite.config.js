0a6ce08135b37679864c3e31947f1488c25b889d
Rodrigo Ferreira Rodrigues <88565286+guigomaster01@users.noreply.github.com>
2025-09-18 10:01:47 -0300

    Update vite.config.js

--- shopping-web/vite.config.js
+++ shopping-web/vite.config.js
@@ -5,16 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_PATH || "/lista-de-compras",
-  // server: {
-  //   host: true,       // para acessar via rede local
-  //   port: 5173,       // se quer travar nessa porta
-  //   strictPort: true, // falha se estiver ocupada
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://127.0.0.1:8000',
-  //       changeOrigin: true,
-  //       rewrite: p => p.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+  server: {
+    host: true,       // para acessar via rede local
+    port: 5173,       // se quer travar nessa porta
+    strictPort: true, // falha se estiver ocupada
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: p => p.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
