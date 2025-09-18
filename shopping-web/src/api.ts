eed428e1bca4c78172e832ab9aceeacc685640b3
Rodrigo Ferreira Rodrigues <88565286+guigomaster01@users.noreply.github.com>
2025-09-18 20:40:57 -0300

    Update api.ts
    Env update at vercel

--- shopping-web/src/api.ts
+++ shopping-web/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 export const api = axios.create({
  // baseURL: "http://127.0.0.1:8000", // URL do backend em produção
     // baseURL: "/api", // URL do backend em desenvolvimento (com proxy)
-    baseURL: "https://lista-de-compras-ten-hazel.vercel.app/", // URL do backend em produção
+    baseURL: import.meta.env.VITE_API_URL, // URL do backend em produção
     timeout: 8000, // 8 segundos
     withCredentials: false, // se precisar enviar cookies
 });
